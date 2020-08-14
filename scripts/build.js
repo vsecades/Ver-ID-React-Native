9a16d6ffd4e80e5f0bad4f03ac11b337c741f455
Jonathan Matus Moreno <jmatus@qxdev.com>
2020-08-14 13:00:57 -0600

    added command to start metro server and to run fix for metro server on android devices

--- scripts/build.js
+++ scripts/build.js
@@ -13,11 +13,15 @@ const SELECTION_OPTIONS = [
     { name: 'Build Plugin Project', value: 0 },
     { name: 'Build React Native Sample 0.63', value: 1 },
     { name: 'Run React Native Sample 0.63 on iOS', value: 2 },
+    { name: 'Start Metro server', value: 3 },
+    { name: 'Fix metro server Android', value: 4 },
 ];
 const commands = {
     0: 'yarn build-project',
     1: `yarn build-project && ${getSampleCommand('reactNativeSample0_63')}`,
     2: 'yarn --cwd samples/reactNativeSample0_63 ios',
+    3: 'yarn --cwd samples/reactNativeSample0_63 start',
+    4: 'adb reverse tcp:8081 tcp:8081',
 };
 
 inquirer
